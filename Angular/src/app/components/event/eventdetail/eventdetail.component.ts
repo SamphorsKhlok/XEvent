2486b3ede8a0796e3f99b3e7c50d5f6c35c6cdbe
Samphors Khlok <khlok.samphors@gmail.com>
2017-09-26 17:56:40 -0500

    update event page
    - connect to backend data
    - component list view talk to detail view
    - detail view form completed
    - using bluebird Promise instead of default mongoose deprecated promise

--- Angular/src/app/components/event/eventdetail/eventdetail.component.ts
+++ Angular/src/app/components/event/eventdetail/eventdetail.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {Component, Input, OnInit} from '@angular/core';
+import { FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {HttpService} from "../../../services/http.service";
 
 @Component({
   selector: 'app-eventdetail',
@@ -8,19 +9,44 @@ import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 })
 export class EventdetailComponent implements OnInit {
   myForm: FormGroup;
-  constructor(private fb: FormBuilder) {
+  @Input() selectedItem ;
+
+  constructor(private fb: FormBuilder, private http: HttpService) {
     this.createForm();
   }
 
   createForm() {
     this.myForm = this.fb.group({
       name: ['', Validators.required ],
+      description: ['',Validators.required],
+      remark: ['',Validators.required],
+      date: ['',Validators.required],
       address: this.fb.group({
+        street: ['',Validators.required],
         city: ['',Validators.required],
-        //state: '',
-        zip: ['',Validators.required]
+        state: ['',Validators.required],
+        zipcode: ['',Validators.required],
       }),
+      location: ['',Validators.required],
+      tags: ['',Validators.required],
+      users: ['',Validators.required],
     });
+  };
+
+  submitForm(){
+    this.http.addEvent().subscribe(
+      (data)=> console.log(data),
+      (error)=> console.error(error),
+      ()=> console.info("completed")
+    );
+  }
+
+  deleteEvent(id){
+    this.http.deleteEvent(id).subscribe(
+      (data)=> console.log(data),
+      (error)=> console.error(error),
+      ()=> console.info("completed")
+    );
   }
 
   ngOnInit() {
