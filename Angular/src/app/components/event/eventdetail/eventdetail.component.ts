162e049585d4fd4c30825e7d493a2fe783258093
Samphors Khlok <khlok.samphors@gmail.com>
2017-09-28 01:52:30 -0500

    fix dialog error

--- Angular/src/app/components/event/eventdetail/eventdetail.component.ts
+++ Angular/src/app/components/event/eventdetail/eventdetail.component.ts
@@ -1,5 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
-import { FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 import {HttpService} from "../../../services/http.service";
 import {MdSnackBar} from "@angular/material";
 
@@ -15,29 +15,46 @@ export class EventdetailComponent implements OnInit {
   constructor(private fb: FormBuilder,
               private http: HttpService,
               public snackBar: MdSnackBar) {
-    this.createForm();
+
   }
 
   createForm() {
-    this.myForm = this.fb.group({
-      name: ['', Validators.required ],
-      description: ['',Validators.required],
-      remark: ['',Validators.required],
-      date: ['',Validators.required],
-      address: this.fb.group({
-        street: ['',Validators.required],
-        city: ['',Validators.required],
-        state: ['',Validators.required],
-        zipcode: ['',Validators.required],
-      }),
-      location: ['',Validators.required],
-      tags: ['',Validators.required],
-      users: ['',Validators.required],
+    // this.myForm = this.fb.group({
+    //   name: ['', Validators.required ],
+    //   description: ['',Validators.required],
+    //   remark: ['',Validators.required],
+    //   date: ['',Validators.required],
+    //   address: this.fb.group({
+    //     street: ['',Validators.required],
+    //     city: ['',Validators.required],
+    //     state: ['',Validators.required],
+    //     zipcode: ['',Validators.required],
+    //   }),
+    //   location: ['',Validators.required],
+    //   tags: ['',Validators.required],
+    //   users: ['',Validators.required],
+    // });
+
+    this.myForm = new FormGroup({
+      name : new FormControl(this.selectedItem.name ||'', Validators.required),
+      description : new FormControl(this.selectedItem.description || "", Validators.required),
+      remark : new FormControl(this.selectedItem.remark || "", Validators.required),
+      date : new FormControl(this.selectedItem.date || "", Validators.required),
+      address : new FormGroup({
+        street: new FormControl( this.selectedItem.address.street || "",Validators.required),
+        city: new FormControl( this.selectedItem.address.city || "",Validators.required),
+        state: new FormControl( this.selectedItem.address.state || "",Validators.required),
+        zipcode: new FormControl( this.selectedItem.address.zipcode || "",Validators.required),
+        }),
+      location : new FormControl(this.selectedItem.location || "", Validators.required),
+      tags : new FormControl(this.selectedItem.tags || "", Validators.required),
+      users : new FormControl(this.selectedItem.users || "",  Validators.required)
     });
   };
 
   submitForm(){
-    this.http.addEvent().subscribe(
+    console.log(this.myForm.value);
+    this.http.saveEvent(this.myForm.value, this.selectedItem._id).subscribe(
       (data)=> console.log(data),
       (error)=> console.error(error),
       ()=> console.info("completed")
@@ -46,6 +63,11 @@ export class EventdetailComponent implements OnInit {
 
   //TODO: form need to fix on page when scroll down far
   ngOnInit() {
+    this.createForm();
+  }
+
+  ngOnChanges(){
+    this.createForm();
   }
 
 }
